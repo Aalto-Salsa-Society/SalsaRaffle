98d9f52cdca8f359ad92af35ba4f4739267737bb
Menno <menno1337@gmail.com>
2024-11-06 20:25:08 +0200

    refactor: simplify generation structure

--- scripts/datagen.py
+++ scripts/datagen.py
@@ -1,3 +1,5 @@
+"""Generate input to run salsaraffle with."""
+
 import random
 from pathlib import Path
 
@@ -5,26 +7,20 @@ import polars as pl
 from faker import Faker
 from xlsxwriter import Workbook
 
+from salsaraffle.column import Col
 from salsaraffle.registration import REGISTRATION_COLUMNS
+from salsaraffle.settings import GROUP_INFO
 
-OUTPUT_DIR = Path("data/generated_input/")
-MEMBER_FILENAME = "members.xlsx"
-RESPONSES_FILENAME = "responses.xlsx"
+OUTPUT_DIR = Path("data") / "generated_input"
+MEMBERS_PATH = OUTPUT_DIR / "Members.xlsx"
+RESPONSES_PATH = OUTPUT_DIR / "responses.xlsx"
 
 NUM_MEMBERS = 300
 NUM_APPROVED = 270
-NUM_PAID = 280
-NUM_REG = 140
+NUM_PAID = NUM_APPROVED - 10
+NUM_REGISTRATIONS = 140
 
-
-CLASSES = [
-    "Salsa Level 1",
-    "Salsa Level 2",
-    "Salsa Level 3",
-    "Salsa Level 4",
-    "Bachata Level 1",
-    "Bachata Level 2",
-]
+CLASSES = [info[0] for info in GROUP_INFO]
 # first choice class preference distribution
 CLASS_DIST = [
     0.3,
@@ -40,113 +36,97 @@ ROLE_DIST = [0.6, 0.4]  # follower vs leader ratio
 
 SECOND_PREF_OPTIONS = ["Yes", "No"]
 # Has second pref vs doesn't have second pref ratio
-SECOND_PREF_DIST = [
-    0.3,
-    0.7,
-]
+SECOND_PREF_DIST = [0.3, 0.7]
 
 
 def main() -> None:
     """Generate data and write data to files."""
-    df = gen_data()
-    write_members_file(df)
-    write_responses_file(df)
+    responses = gen_responses()
+    write_responses_file(responses)
+
+    members = gen_members(responses)
+    write_members_file(members)
 
 
-def gen_data() -> pl.DataFrame:
-    """Return a df of data needed to generate member and responses excels."""
+def gen_responses() -> pl.DataFrame:
+    """Generate fake responses."""
     fake = Faker()
 
-    names = [
-        fake.name() for x in range(NUM_MEMBERS)
-    ]  # uses Faker to generate fake names
-    emails = [
-        name.split(" ")[0] + "." + name.split(" ")[1] + "@email.com"
-        for name in names
-    ]
+    names = [fake.name() for _ in range(NUM_MEMBERS)]
+    emails = [".".join(name.split()).lower() + "@email.com" for name in names]
     handles = ["@" + name.replace(" ", "").lower() for name in names]
-    approved = [True for x in range(NUM_APPROVED)] + [
-        False for x in range(NUM_MEMBERS - NUM_APPROVED)
-    ]
-    paid = [True for x in range(NUM_PAID)] + [
-        False for x in range(NUM_MEMBERS - NUM_PAID)
-    ]
     class_pref = random.choices(CLASSES, weights=CLASS_DIST, k=NUM_MEMBERS)
     role_pref = random.choices(ROLES, weights=ROLE_DIST, k=NUM_MEMBERS)
     has_second_pref = random.choices(
         SECOND_PREF_OPTIONS, weights=SECOND_PREF_DIST, k=NUM_MEMBERS
     )
 
-    col_names = list(REGISTRATION_COLUMNS.keys())
-
     df = pl.DataFrame(
         {
-            col_names[0]: handles,
-            col_names[1]: names,
-            col_names[2]: emails,
-            col_names[3]: class_pref,
-            col_names[4]: role_pref,
-            col_names[5]: has_second_pref,
-            "approved": approved,
-            "paid": paid,
+            Col.HANDLE: handles,
+            Col.NAME: names,
+            Col.EMAIL: emails,
+            Col.P1_CLASS: class_pref,
+            Col.P1_ROLE: role_pref,
+            Col.HAS_P2: has_second_pref,
         }
     )
 
-    return gen_second_pref_data(df, col_names)
+    df = df.with_columns(
+        (
+            pl.when(pl.col(Col.HAS_P2) == "Yes")
+            .then(pl.Series(random.choices(CLASSES, k=df.height)))
+            .otherwise(pl.lit(None))
+            .alias(Col.P2_CLASS)
+        ),
+        (
+            pl.when(pl.col(Col.HAS_P2) == "Yes")
+            .then(pl.Series(random.choices(ROLES, k=df.height)))
+            .otherwise(pl.lit(None))
+            .alias(Col.P2_ROLE)
+        ),
+        (
+            pl.when(pl.col(Col.HAS_P2) == "Yes")
+            .then(pl.Series(random.choices([True, False], k=df.height)))
+            .otherwise(pl.lit(None))
+            .alias(Col.ONLY_1)
+        ),
+    )
+
+    inv_registration_columns = {v: k for k, v in REGISTRATION_COLUMNS.items()}
+    return df.rename(inv_registration_columns)
 
 
-def gen_second_pref_data(df: pl.DataFrame, col_names: list) -> pl.DataFrame:
-    """Generate data for students that selected a second preference."""
-    second_class, second_role, both_pref = [], [], []
+def write_responses_file(df: pl.DataFrame) -> None:
+    """Write response data to excel file."""
+    OUTPUT_DIR.mkdir(parents=True, exist_ok=True)
+    with Workbook(RESPONSES_PATH) as wb:
+        df.sample(NUM_REGISTRATIONS, shuffle=True).write_excel(workbook=wb)
 
-    for row in df.iter_rows():
-        if row[5] == "Yes":
-            classes = CLASSES.copy()
-            classes.remove(row[3])
-            second_class.append(
-                str(random.choice(classes))
-            )  #randomnly pick a class that isn't the first class
-            second_role.append(
-                str(random.choice(ROLES))
-            )  # randomnly pick a role, even distribution
-            both_pref.append(
-                random.choice([True, False])
-            )  # randomnly pick true or false, even distribution
-        else:
-            second_class.append(None)
-            second_role.append(None)
-            both_pref.append(None)
 
-    return df.select(
-        [
-            pl.all(),
-            pl.lit(pl.Series(second_class)).alias(col_names[6]),
-            pl.lit(pl.Series(second_role)).alias(col_names[7]),
-            pl.lit(pl.Series(both_pref)).alias(col_names[8]),
-        ]
+def gen_members(responses: pl.DataFrame) -> pl.DataFrame:
+    """Generate fake members."""
+    handle_map = next(
+        {k: v} for k, v in REGISTRATION_COLUMNS.items() if v == Col.HANDLE
+    )
+    approved = [True] * NUM_APPROVED + [False] * (NUM_MEMBERS - NUM_APPROVED)
+    paid = [True] * NUM_PAID + [False] * (NUM_MEMBERS - NUM_PAID)
+    return (
+        responses.select(handle_map)
+        .rename(handle_map)
+        .sample(fraction=1, shuffle=True)
+        .with_columns(
+            pl.Series(approved).alias(Col.APPROVED),
+            pl.Series(paid).alias(Col.PAID),
+        )
     )
 
 
 def write_members_file(df: pl.DataFrame) -> None:
     """Write member data to excel file."""
-    if not Path.is_dir(OUTPUT_DIR):
-        Path.mkdir(OUTPUT_DIR)
-
-    with Workbook(OUTPUT_DIR / MEMBER_FILENAME) as wb:
-        df.select(["Telegram handle", "approved", "paid"]).rename(
-            {"Telegram handle": "handle"}
-        ).write_excel(workbook=wb)
-
-
-def write_responses_file(df: pl.DataFrame) -> None:
-    """Write response data to excel file."""
-    if not Path.is_dir(OUTPUT_DIR):
-        Path.mkdir(OUTPUT_DIR)
-
-    with Workbook(OUTPUT_DIR / RESPONSES_FILENAME) as wb:
-        df.sample(NUM_REG).select(
-            [pl.all().exclude(["approved", "paid"])]
-        ).write_excel(workbook=wb)
+    OUTPUT_DIR.mkdir(parents=True, exist_ok=True)
+    with Workbook(MEMBERS_PATH) as wb:
+        df.sample(NUM_MEMBERS, shuffle=True).write_excel(workbook=wb)
 
 
 if __name__ == "__main__":
